a63f6f21d537f3f4bc4a3d022de0c7dfb8332ff7
KKJ <k1992013@naver.com>
2024-03-18 17:42:50 +0900

    3/18 파이썬

---
+++ pythonProject/기본연습.py
@@ -0,0 +1,28 @@
+# 파이썬의 주석
+
+name = "김기주"
+email = "k1992013@naver.com"
+position = "학생"
+addr = "경기도 부천시 작동 122-2"
+
+print(f"""
+주소 : {addr}
+직책 : {position}
+이름 : {name}
+메일 : {email}
+""")
+
+if True :
+    print("정상")
+else:
+    print("에러")
+
+"""
+작성자 : 김기주
+목적 : 파이썬 연습용 프로그램
+날짜 : 2024.03.18
+"""
+
+print(38) #숫자 출력
+print("문자열 출력")
+print([1,2,3])
