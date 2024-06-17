20bf5c47706fc0dc005ef4c8d9d949ae93a620b7
KKJ <k1992013@naver.com>
2024-06-17 15:39:52 +0900

    크롤링 연습?

--- pythonProject/요청과응답.py
+++ pythonProject/요청과응답.py
@@ -7,11 +7,16 @@ html_doc = '''
   <head>
     <title>Example Page</title>
   </head>
-  <body>
+  <body id = "container">
+  <div class="example">여기는 example인 div 태그 입니다. </div>
     <h1>Hello, Beautiful Soup!</h1>
     <div class="content">
       <p>This is a paragraph.</p>
       <p>This is another paragraph.</p>
+    </div>
+    <div id = "parent">
+    <div class="child">첫 번째 자식</div>
+    <div class="child">두 번째 자식</div>
     </div>
     <a href="https://www.example.com">Link</a>
   </body>
@@ -36,3 +41,29 @@ for a in a_tags:
     print(a)
 
 # Tag 객체 다루기
+
+# Tag 객체에서 요소의 이름 얻기
+tag_name = title_tag.name
+print(tag_name)  # 'title'
+
+# Tag 객체에서 요소의 속성 얻기
+tag_attrs = div_tags[0].attrs
+print(tag_attrs)  # {'class': ['content']}
+
+# Tag 객체에서 요소의 내용 얻기
+tag_content = div_tags[0].text
+print(tag_content)  # 'This is a paragraph.\nThis is another paragraph.'
+
+div_tags =soup.select('div.example')
+print(div_tags)
+
+#id 선택자 사용하기
+id_sel = soup.select_one("#container")
+print(id_sel)
+
+children = soup.select("#parent >.child")
+print(children)
+
+attrs = {"class" : "example"}
+el = soup.find_all(attrs=attrs)
+print(el)
